65044174ccdfc4fc8193a95e1d1874f42e57783f
TuanDv <vinhtuan9668@gmail.com>
2023-06-25 14:47:16 +0700

    commit tuandv
    commit tuandv

--- SWP391-Team5/src/java/dao/requestDAO.java
+++ SWP391-Team5/src/java/dao/requestDAO.java
@@ -4,20 +4,19 @@
  */
 package dao;
 
-import java.sql.Connection;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import model.Request;
 import model.Skill;
-import model.User;
+
 
 /**
  *
