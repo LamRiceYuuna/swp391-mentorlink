7d00f906ca76fd3db5b246bcdc9d8569f236cfcc
LAMRICE <lamhllm24@gmail.com>
2023-06-26 22:04:03 +0700

    commit
    commit

---
+++ SWP391-Team5/build/web/assets/js/popup.js
@@ -0,0 +1,28 @@
+const openPopupBtn = document.querySelector('[data-modal-target]');
+const closePopupBtn = document.querySelector('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+
+console.log(overlay);
+
+openPopupBtn.addEventListener('click', () => {
+    const modalData = document.querySelector(openPopupBtn.dataset.modalTarget);
+    openModal(modalData);
+});
+overlay.addEventListener('click', () => {
+    const modals = document.querySelector('.popup-btn-rq-statistic.active');
+    closeModal(modals);
+});
+
+
+function openModal(modal) {
+    if(modal === null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if(modal === null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
